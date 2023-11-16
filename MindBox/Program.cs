54375a4a51e8809e0ba12ab5a516a73f493d94b3
algamach <123144931+algamach@users.noreply.github.com>
2023-11-16 15:04:48 +0300

    Initial commit

---
+++ MindBox/Program.cs
@@ -0,0 +1,10 @@
+﻿using static MindBox.GeometryLibrary;
+
+Circle circle = new Circle(5);
+Console.WriteLine($"Площадь круга: {circle.CalculateArea()}");
+
+Triangle triangle = new Triangle(3, 4, 5);
+Console.WriteLine($"Площадь треугольника: {triangle.CalculateArea()}");
+Console.WriteLine($"Прямоугольный треугольник: {triangle.IsRightTriangle()}");
+
+Console.ReadLine();
